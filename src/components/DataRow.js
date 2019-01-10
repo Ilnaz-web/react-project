99542be90e02ece5bc43943376e6acb280fe2bbc
Ilnaz-web <31491324+Ilnaz-web@users.noreply.github.com>
2019-01-10 17:47:07 +0300

    Create scrolling modal

--- src/components/DataRow.js
+++ src/components/DataRow.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactJson from 'react-json-view'
-import JSONViewer from 'react-json-viewer'
+// import ReactJson from 'react-json-view'
+// import JSONViewer from 'react-json-viewer'
 import JSONTree from 'react-json-tree'
 import Modal from './Modal'
 
@@ -18,27 +18,27 @@ class DataRow extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      showModal: false
+      showModal: false,
     }
   }
   
   viewJSON = () => {
     this.setState({showModal: true});
+    document.body.style.overflow = "hidden";
+    
   }
 
   closeModal = () => {
-    this.setState({ showModal: false });
+    this.setState({ showModal: false});
+    document.body.style.overflow = "";
   }
 
   renderModal = () => {
     if (!this.state.showModal) return null;
     let log = this.props.data._source.log;
-    //console.log(log);
     console.log(JSON.parse(log));
   
-    return <Modal onClose={this.closeModal}>
-      {/* <ReactJson src={_objectWithoutProperties(JSON.parse(log), ['httpRequest'])} /> */}
-      {/* <JSONViewer json={JSON.parse(log)} /> */}
+    return <Modal onClose={this.closeModal} >
       <JSONTree data={JSON.parse(log)} />
     </Modal>
   };
@@ -47,7 +47,7 @@ class DataRow extends React.Component {
     const elem = this.props.data;
     return (
       <React.Fragment>
-        <tr key={elem._id} onClick={this.viewJSON}>
+        <tr key={elem._id} onClick={this.viewJSON} id={elem._id} >
           <td>{elem._source.DT}</td>
           <td>{elem._source.reqTS}</td>
           <td>{elem._source.name}</td>
@@ -63,10 +63,8 @@ class DataRow extends React.Component {
         </tr>
         {this.renderModal()}
       </React.Fragment>
-    
   );
   }
-  
 }
 
 export default DataRow
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './components/style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import './components/Modal.css'
 import App from './components/App'
